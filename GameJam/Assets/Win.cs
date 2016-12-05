f576104c1a95e5983f9d6700c00b40c37cecca33
Emily <emmyjenny90@gma>
2016-12-05 18:21:29 +1100

    Added Wn trigger and started player Grab

--- GameJam/Assets/Win.cs
+++ GameJam/Assets/Win.cs
@@ -1,20 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Author: Emily Cochrane
+//Descrition: detects when an Object is inside the Trigger and if all objects are inside then the Game Win's
 public class Win : MonoBehaviour {
+	
+	public string objectsTagName;
+	private int objectsOnLevel;//only set once and can not be changed
+	public int objectsInTrigger = 0;
+	public int objectsRemainingInLevel;
 
 	// Use this for initialization
 	void Start () {
-		
+		//find the number of Objects in level
+		objectsOnLevel = GameObject.FindGameObjectsWithTag (objectsTagName).Length;
 	}
+	//detects if an object is in the Trigger
 	void OnTriggerEnter(Collider other){
-		if (other.tag = "Object") {
-			
+		if (other.tag == objectsTagName) {
+			objectsInTrigger++;
+			objectsRemainingInLevel = objectsOnLevel - objectsInTrigger;
+			GameState.objectsRemaining = objectsRemaingInLevel;
+			//win check
+			if (objectsRemainingInLevel == 0) {
+				GameState.HasWon = true;
+			}
 		}
 	}
-	// Update is called once per frame
-	void Update () {
-		
+	void OnTriggerExit(Collider other){
+		if (other.tag == objectsTagName) {
+			objectsInTrigger--;
+			objectsRemainingInLevel = objectsOnLevel - objectsInTrigger;
+			GameState.objectsRemaining = objectsRemaingInLevel;
+		}
 	}
 }
